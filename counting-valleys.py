e6d750a05fc85992b8939f16febaabeb5eb40c4c
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-12-04 01:59:18 +0530

    Create counting-valleys.py

---
+++ counting-valleys.py
@@ -0,0 +1,41 @@
+# https://www.hackerrank.com/challenges/counting-valleys/problem
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    uc=c=0
+    for i in path:
+        if i=="U":
+            uc+=1
+        else:
+            uc-=1
+        if i=="U" and uc==0:
+            c+=1
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
